66b0800f381f29f2e69bb363bf639d699a395305
gnkyte <gbendici@uci.edu>
2016-06-04 21:19:18 -0700

    Add files via upload

---
+++ utilities/Purchase.java
@@ -0,0 +1,50 @@
+package utilities;
+
+public class Purchase {
+	private String name;
+	private String ship_address;
+	private String bill_address;
+	private String email;
+	private String credit;
+	
+	public Purchase(){
+		name = "";
+		ship_address = "";
+		bill_address = "";
+		email = "";
+		credit = "";
+	}
+	
+	public  String getName(){
+		return name;
+	}
+	
+	public String getShip(){
+		return ship_address;
+	}
+	
+	public String getBill(){
+		return bill_address;
+	}
+	
+	public String getEmail(){
+		return email;
+	}
+	
+	public String getCredit(){
+		return credit;
+	}
+	
+	public void makePurchase(String [] params){
+		name = params[0];
+		credit = params[1];
+		ship_address = params[2];
+		bill_address = params[3];
+		email = params[4];
+	}
+	
+	public String getCreditEnd(){
+		String toReturn = credit.substring(12);
+		return toReturn;
+	}
+}
